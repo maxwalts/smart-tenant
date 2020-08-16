e64ada6d1271dcbc1a6b7b56b4c431713d302e87
mwalts <mwalts@umich.edu>
2020-08-16 02:14:28 -0500

    completed contract v1, presentation notes

--- Lease.sol
+++ Lease.sol
@@ -3,25 +3,28 @@ import "./SafeMath.sol";
 
 contract Lease {
     
-    event agreementReached(
+    /**
+     * Events
+    */ 
+    event AgreementReached(
         address landlord,
         address tenant,
-        address contractAddress
+        string propertyAddress
         
     );
-    
-    enum State{Unsigned, AwaitingDeposit, Active, Closed}
-    State public currentState; //defaults to first value
+    event OfferMade(
+        uint8 term,
+        uint256 rent,
+        uint256 securityDeposit,
+        address sender
+    );
+    event Aborted();
     
     /**
-     * parties
+     * Addresses
     */ 
     address payable public landlord;
     address payable public primaryTenant;
-    
-    mapping (address => uint256) public balances; //key a tenant address to a balance due value
-    mapping (uint8 => bool) public monthsPaid; //key a month (1-12) to true (paid) or false (unpaid)
-    uint8 currentMonth = 1;
     
     /**
      * Structs
@@ -34,45 +37,55 @@ contract Lease {
     Property public property;
     
     struct Offer {
-        uint256 term; //how many months the contract lasts
+        uint8 term; //how many months the contract lasts
         uint256 rent; //monthly rate
         uint securityDeposit; //security deposit is a multiple of the monthly rent
     }
     Offer public offer;
     
     struct CounterOffer {
-        uint256 term;
+        uint8 term;
         uint256 rent;
         uint256 securityDeposit;
     }
     CounterOffer public counterOffer;
     
     /*
-    / Legal
+    / Variables
     */ 
-    //highest multiple allowed for security deposit
-    uint constant internal SD_LIMIT = 2;
+    uint8 constant internal SD_LIMIT = 2; //highest multiple allowed for security deposit
+    uint256 public expiration; //time out the offer before activation if no agreement is met
     
+    mapping (address => uint256) public balancesDue; //key a tenant address to a balance due value
+    mapping (uint8 => bool) public monthsPaid; //key a month (1-12) to true (paid) or false (unpaid)
+    uint8 public currentMonth = 1;
     
-    modifier offerIsValid() {
+    enum State{Unsigned, AwaitingDeposit, Active, Closed}
+    State public currentState; //defaults to first value
+    
+    /*
+    / Modifiers
+    */ 
+    modifier offerIsValid(uint8 _term, uint256 _rent, uint256 _securityDeposit) {
         require(
-            offer.rent > 100 && offer.rent < 10000,
-            "Review offer; a valid rent value is between 100 and 10000."
-        );
-        require(
-            offer.term >= 2 && offer.term <= 12,
+            _term >= 2 && _term <= 12,
             "Review offer; a valid term is between 2 and 12, inclusive."
         );
         require(
-            offer.securityDeposit > 50 && offer.securityDeposit < 20000,
+            _rent > 100 && _rent < 10000,
+            "Review offer; a valid rent value is between 100 and 10000."
+        );
+        require(
+            _securityDeposit > 50 && _securityDeposit < 20000,
             "Review offer; a valid security deposit is between 50 and 20000."
         );
         require (
-            offer.securityDeposit <= SafeMath.mul(offer.rent, SD_LIMIT), 
+            _securityDeposit <= SafeMath.mul(_rent, SD_LIMIT), 
             "Security deposit must not exceed twice the monthly rent."
         );
         _;
     }
+    
     modifier onlyLandlord() {
         require (
             msg.sender == landlord, 
@@ -88,11 +101,6 @@ contract Lease {
         _;
     }
     
-    modifier condition(bool _condition) { //use this in the future to cut down code length.
-        require(_condition);
-        _;
-    }
-    
     modifier inState(State _state) {
         require(
             currentState == _state,
@@ -101,27 +109,75 @@ contract Lease {
         _;
     }
     
-    function makeOffer(uint _term, uint _rent, uint _securityDeposit) 
+    /*
+    / Constructor
+    */ 
+    constructor(address payable _primaryTenant, uint256 duration) 
+        public
+        payable
+    {
+        primaryTenant = _primaryTenant;
+        landlord = msg.sender;
+        expiration = SafeMath.add(block.timestamp, duration);
+    }
+    
+    /*
+    / onlyLandlord functions
+    */ 
+    function setProperty(string memory _streetAddress, uint256 _numBeds, uint256 _sqFt) 
+        public 
+        onlyLandlord
+    {
+        property.streetAddress = _streetAddress;
+        property.numBeds = _numBeds;
+        property.sqFt = _sqFt;
+    }
+    
+    function makeOffer(uint8 _term, uint _rent, uint _securityDeposit) 
         public 
         onlyLandlord 
-        offerIsValid
+        offerIsValid(_term, _rent, _securityDeposit)
         inState(State.Unsigned)
     {
-        
         offer.term = _term;
         offer.rent = _rent;
         offer.securityDeposit = _securityDeposit;
+
+        emit OfferMade(_term, _rent, _securityDeposit, landlord);
     }
 
-    function makeCounterOffer(uint _term, uint _rent, uint _securityDeposit) 
+    function abort() //landlord can abort the contract before it has been signed, for example of they find another tenant.
+        public
+        onlyLandlord
+        inState(State.Unsigned)
+    {
+        currentState = State.Closed;
+        emit Aborted();
+        selfdestruct(msg.sender);
+    }
+    
+    function extendContract(uint256 timeToAdd) /// the sender can extend the expiration at any time
+        public 
+        onlyLandlord
+        inState(State.Unsigned)
+    {
+        expiration = SafeMath.add(expiration, timeToAdd);
+    }
+    
+    /*
+    / onlyTenant functions
+    */ 
+    function makeCounterOffer(uint8 _term, uint _rent, uint _securityDeposit) 
         public 
         onlyTenant 
-        offerIsValid
+        offerIsValid(_term, _rent, _securityDeposit)
         inState(State.Unsigned)
     {
         counterOffer.term = _term;
         counterOffer.rent = _rent;
         counterOffer.securityDeposit = _securityDeposit;
+
+        emit OfferMade(_term, _rent, _securityDeposit, primaryTenant);
     }
     
     function sign()
@@ -132,15 +188,34 @@ contract Lease {
         require(offer.term == counterOffer.term);
         require(offer.rent == counterOffer.rent);
         require(offer.securityDeposit == counterOffer.securityDeposit);
+        
+        require(block.timestamp < expiration); //contract must be signed before the expiration date
+        
         currentState = State.AwaitingDeposit;
+        balancesDue[primaryTenant] = offer.securityDeposit;
+        
+        emit AgreementReached(landlord, primaryTenant, property.streetAddress);
+
     }
-    
-    function paySD()
+
+    function paySD() //activates the contract
         public
         payable
         onlyTenant
         inState(State.AwaitingDeposit)
     {
+        require(
+            msg.value == offer.securityDeposit, 
+            "you must pay the security deposit exactly."
+        );
+        
+        require(
+            balancesDue[primaryTenant] == offer.securityDeposit, 
+            "balance due does not equal security deposit."
+        );
+        landlord.transfer(balancesDue[primaryTenant]);
+        balancesDue[primaryTenant] = offer.rent;
+        
         currentState = State.Active;
     }
     
@@ -150,18 +225,26 @@ contract Lease {
         onlyTenant
         inState(State.Active)
     {
+        require(
+            msg.value == offer.rent, 
+            "you must pay the rent exactly."
+        );
+
+        require(
+            balancesDue[primaryTenant] == offer.rent, 
+            "balance due does not equal rent."
+        );
+        landlord.transfer(balancesDue[primaryTenant]);
+        balancesDue[primaryTenant] = offer.rent;
+        
         monthsPaid[currentMonth] = true;
         currentMonth++;
         
         if (currentMonth > offer.term) {
+            balancesDue[primaryTenant] = 0;
             currentState = State.Closed;
         }
     }
     
-
-    constructor(address payable _primaryTenant) public {
-        primaryTenant = _primaryTenant;
-        landlord = msg.sender;
-    }
     
 }
---
+++ README.md
@@ -0,0 +1,44 @@
+# house-me-now 
+A smart contract to handle a lease agreement between two parties. 
+Written for Blockchain@Michigan, August 2020.
+
+### Rules
+- Parties can only negotiate the contract before it has been signed
+- Rent is paid at the very end of a month.
+- Contract closes after last rent payment or upon abort()
+
+### Organization
+- One __tenant__ payable address
+- One __landlord__ payable address
+- __Property__ struct
+- __Offer__ struct
+- __CounterOffer__ struct
+- mapping __balances__ to keep track of amount due
+
+### Functions
+- __onlyTenant:__
+	- makeCounterOffer()
+	- sign() - initializes the contract
+	- paySD()
+	- payRent()
+
+- __onlyLandlord:__
+	- *Constructor()*
+	- setProperty()
+	- makeOffer()
+	- abort()
+	- extendContract()
+
+- __Open:__
+	- view contract attributes with inherent getter functions
+
+### States
+- enum State{Unsigned, AwaitingDeposit, Active, Closed}
+	- State.Unsigned: contract is awaiting an agreement between Offer and CounterOffer
+	- State.AwaitingDeposit: tenant has signed but has not submitted a deposit
+	- State.Active: the lease is active, rent is due at the end of every month
+	- State.Closed: contract term has completed, or contract has been voided by the landlord.
+
+### Testing
+
+### Future
